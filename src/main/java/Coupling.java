24b1b995fd8da86f7d4ae851123489fbc37f82a6
gajan <gajananmchaudhari@gmail.com>
2022-08-31 22:09:59 +0530

    Hellow

---
+++ src/main/java/Coupling.java
@@ -0,0 +1,16 @@
+
+public class Coupling {
+
+	
+	public static void main(String[] args) {
+		
+			int a=10;
+			
+			System.out.println(a);
+			Integer b=a;
+			
+			System.out.println(b);
+
+	}
+
+}
